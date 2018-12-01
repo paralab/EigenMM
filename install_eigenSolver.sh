8a273c33aab16b192ab34ccea5f2f931098377dd
majidrp <u1013493@utah.edu>
2018-12-01 01:47:02 -0700

    minor

--- install_eigenSolver.sh
+++ install_eigenSolver.sh
@@ -6,7 +6,8 @@ module load petsc/3.8.3
 tar -xzf slepc-3.8.3.tar.gz
 cd slepc-3.8.3
 export SLEPC_DIR=`pwd`
-#./configure
-#make
+./configure
+make
+
 echo "copy the following line inside CMakeLists.txt after the line that reads: paste the SLEPC_DIR address here:"
 echo "set(SLEPC_DIR `pwd`)"
