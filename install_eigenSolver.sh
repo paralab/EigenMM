c3093e08b4cf591381dc7db8b74d71cb20af6016
majidrp <u1013493@utah.edu>
2018-12-01 01:26:19 -0700

    update install_eigenSolver.sh

--- install_eigenSolver.sh
+++ install_eigenSolver.sh
@@ -3,9 +3,6 @@
 module load mumps/5.0.2
 module load petsc/3.8.3
 
-git clone https://github.com/paralab/eigenSolver.git
-cd eigenSolver
-
 tar -xzf slepc-3.8.3.tar.gz
 cd slepc-3.8.3
 export SLEPC_DIR='pwd'
